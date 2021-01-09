e088b2b3afabc61ada3c031556f55786d744b1ab
amcourson <amcourson09@sbcglobal.net>
2021-01-08 19:11:50 -0600

    updating password generator

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var characters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+var characterNum = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+                    "0","1","2","3","4","5","6","7","8","9"];
+
+var upperNum = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+                "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+                "0","1","2","3","4","5","6","7","8","9"];
 
 // Write password to the #password input
 function writePassword() {
@@ -12,3 +20,29 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePasword(){
+  var charLength = prompt("How many characters do you want included in your new password?");
+  var num = confirm ("Do you want numbers in your password?");
+
+let password = ' ';
+
+console.log(charLength);
+console.log(num);
+
+if(charLength === true && num === true) {
+  for(var i = 0; i <charLength; i++){
+    charPick = upperNum[Math.floor(Math.random()*upperNum.length)];
+    console.log(charPick);
+    password = password.toString()+charPick.toString();
+    console.log(password);
+  }
+}
+
+
+
+return password;
+}
+
+
+
